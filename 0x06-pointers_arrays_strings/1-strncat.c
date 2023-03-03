cb05ca485b3843db15d1452e8e87555148756742
SalanoKieran <priscillammbone@gmail.com>
2023-03-02 17:22:10 -0800

    No.1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * using at most n bytes from src
- * @dest: input value
- * @src: input value
- * @n: input value
+ * _strcat - concatenates two strings with n bytes
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of char to be copied
  * Return: dest
  */
 char *_strcat(char *dest, char *src, int n)
